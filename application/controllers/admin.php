65c7e69f7c854b0d86f565e383ea1e2f83f92a40
Nguyen Thuc <me@ngthuc.com>
2017-11-21 15:54:09 +0700

    Update master layout

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,13 +1,19 @@
 <?php
 
-class Admin extends CI_Controller{
+class Category extends CI_Controller{
     public function __construct() {
         parent::__construct();
-        $this->load->helper("url");
     }
 
-    public function index(){
+    public function add(){
         $data['subview'] = 'admin/index_view';
+        $data['info'] =  array(
+            'name' => 'Hasegawa kaito',
+            'website' => 'freetuts.net',
+            'email' => 'hoaiminhit1990@gmail.com',
+            'phone' => '1234567894556',
+        );
+        $data['title'] = 'Add A Category';
         $this->load->view('admin/main', $data);
     }
 }
