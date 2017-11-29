64988ceeb85e2e9a5654e39db1a40beddc029bec
Nguyen Thuc <me@ngthuc.com>
2017-11-29 11:34:45 +0700

    CRUD

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,105 +1,145 @@
 <?php
-class User extends CI_Controller{
-    public function __construct(){
+// class User extends CI_Controller{
+//     public function __construct(){
+//         parent::__construct();
+//         // $this->load->library("database");
+//         $this->load->database();
+//     // }
+//
+//     public function index(){
+//         $query=$this->db->get("user");
+//         $data=$query->result_array();
+//         echo "<pre>";
+//         print_r($data);
+//         echo "</pre>";
+//     }
+//
+//     public function index(){
+//         $query=$this->db->get("user");
+//         $data=$query->row_array();
+//         echo "<pre>";
+//         print_r($data);
+//         echo "</pre>";
+//     }
+//
+//     public function index(){
+//         $this->db->select("id, username, level");
+//         $query=$this->db->get("user");
+//         $data=$query->result_array();
+//         echo "<pre>";
+//         print_r($data);
+//         echo "</pre>";
+//       }
+//
+//     public function index(){
+//         $this->db->select("id, username, level");
+//         $this->db->order_by("id", "desc");
+//         $query=$this->db->get("user");
+//         $data=$query->result_array();
+//         echo "<pre>";
+//         print_r($data);
+//         echo "</pre>";
+//       }
+//
+//     public function index(){
+//         $this->db->select("id, username, level");
+//         $this->db->order_by("id", "desc");
+//         $this->db->limit(2, 0);
+//         $query=$this->db->get("user");
+//         $data=$query->result_array();
+//         echo "<pre>";
+//         print_r($data);
+//         echo "</pre>";
+//       }
+//
+//     public function index(){
+//     //     // $this->db->select("id, username, level");
+//     //     // $this->db->order_by("id", "desc");
+//     //     // $this->db->limit(7, 0);
+//     //     // $this->db->where("level", "2");
+//     //     // $this->db->select_min("id");
+//     //     $query=$this->db->get("user");
+//     //     $data=$query->result_array();
+//     //     echo "<pre>";
+//     //     print_r($data);
+//     //     echo "</pre>";
+//     //   }
+//     //
+//     // public function index2(){
+//     //     $data=array(
+//     //         "username" => "kaito",
+//     //         "password" => "1212445",
+//     //         "level"    => "2",
+//     //     );
+//     //     $this->db->insert("user", $data);
+//     //   }
+//     //
+//     // public function index3(){
+//     //     $data=array(
+//     //         "username" => "kaito",
+//     //         "password" => "kaito123",
+//     //         "level"    => "1",
+//     //     );
+//     //     $this->db->where("id", "2");
+//     //     if($this->db->update("user", $data)){
+//     //         echo "Update Thanh cong";
+//     //     }else{
+//     //         echo "Update That bai";
+//     //     }
+//     //   }
+//     //
+//     // public function index4($id){
+//     //       $this->db->where("id", $id);
+//     //       if($this->db->delete("user")){
+//     //           echo "Xoa thanh cong";
+//     //       }else{
+//     //           echo "Xoa that bai";
+//     //       }
+//     //   }
+//     //
+//     // public function index5(){
+//     //     $this->load->model("Muser");
+//     //     $data['data']=$this->Muser->listUser();
+//     //     $this->load->view("user/list_view", $data);
+//     //   }
+// }
+    class User extends CI_Controller {
+
+    protected $_data;
+
+    public function __construct() {
         parent::__construct();
-        // $this->load->library("database");
-        $this->load->database();
+        $this->load->helper('url');
     }
 
-    // public function index(){
-    //     $query=$this->db->get("user");
-    //     $data=$query->result_array();
-    //     echo "<pre>";
-    //     print_r($data);
-    //     echo "</pre>";
+    // public function index() {
+    //     $this->_data['subview'] = 'user/index_view';
+    //     $this->_data['titlePage'] = 'List All User';
+    //     $this->_data['info'] = $this->Muser->getList();
+    //     $this->_data['total_user'] = $this->Muser->countAll();
+    //     $this->load->view('user/main.php', $this->_data);
     // }
 
-    // public function index(){
-    //     $query=$this->db->get("user");
-    //     $data=$query->row_array();
-    //     echo "<pre>";
-    //     print_r($data);
-    //     echo "</pre>";
-    // }
+    public function index() {
+       $this->_data['subview'] = 'user/index_view';
+       $this->_data['titlePage'] = 'List All User';
+       $this->load->model('Muser');
 
-    // public function index(){
-    //     $this->db->select("id, username, level");
-    //     $query=$this->db->get("user");
-    //     $data=$query->result_array();
-    //     echo "<pre>";
-    //     print_r($data);
-    //     echo "</pre>";
-    //   }
+       $this->_data['info'] = $this->Muser->getList();
+       $this->_data['total_user'] = $this->Muser->countAll();
+       $this->load->view('user/main.php', $this->_data);
+   }
 
-    // public function index(){
-    //     $this->db->select("id, username, level");
-    //     $this->db->order_by("id", "desc");
-    //     $query=$this->db->get("user");
-    //     $data=$query->result_array();
-    //     echo "<pre>";
-    //     print_r($data);
-    //     echo "</pre>";
-    //   }
-
-    // public function index(){
-    //     $this->db->select("id, username, level");
-    //     $this->db->order_by("id", "desc");
-    //     $this->db->limit(2, 0);
-    //     $query=$this->db->get("user");
-    //     $data=$query->result_array();
-    //     echo "<pre>";
-    //     print_r($data);
-    //     echo "</pre>";
-    //   }
-
-    public function index(){
-        // $this->db->select("id, username, level");
-        // $this->db->order_by("id", "desc");
-        // $this->db->limit(7, 0);
-        // $this->db->where("level", "2");
-        // $this->db->select_min("id");
-        $query=$this->db->get("user");
-        $data=$query->result_array();
-        echo "<pre>";
-        print_r($data);
-        echo "</pre>";
-      }
-
-    public function index2(){
-        $data=array(
-            "username" => "kaito",
-            "password" => "1212445",
-            "level"    => "2",
-        );
-        $this->db->insert("user", $data);
-      }
-
-    public function index3(){
-        $data=array(
-            "username" => "kaito",
-            "password" => "kaito123",
-            "level"    => "1",
-        );
-        $this->db->where("id", "2");
-        if($this->db->update("user", $data)){
-            echo "Update Thanh cong";
-        }else{
-            echo "Update That bai";
+   public function del($id) {
+        $this->load->model('Muser');
+        $this->db->where("id", $id);
+        if($this->db->delete("user")){
+           echo "Xoa thanh cong";
+        } else{
+           echo "Xoa that bai";
         }
-      }
-
-    public function index4($id){
-          $this->db->where("id", $id);
-          if($this->db->delete("user")){
-              echo "Xoa thanh cong";
-          }else{
-              echo "Xoa that bai";
-          }
-      }
-
-    public function index5(){
-        $this->load->model("Muser");
-        $data['data']=$this->Muser->listUser();
-        $this->load->view("user/list_view", $data);
-      }
+        // $this->Muser->deleteUser($id);
+        $this->session->set_flashdata("flash_mess", "Delete Success");
+        redirect(base_url() . "user");
+ }
 }
--- application/models/muser.php
+++ application/models/muser.php
@@ -1,24 +1,43 @@
 <?php
+// class Muser extends CI_Model{
+//     public function __construct(){
+//         parent::__construct();
+//         $this->load->database();
+//     }
+//
+//     public function listUser(){
+//         $this->db->select("id, username, level");
+//         $query=$this->db->get("user");
+//         return $query->result_array();
+//     }
+//
+//     public function countAll(){
+//         return $this->db->count_all("user");
+//     }
+//
+//     public function getList($total, $start){
+//        $this->db->limit($total, $start);
+//        $query=$this->db->get("user");
+//        return $query->result_array();
+// }
+// }
+
 class Muser extends CI_Model{
-    public function __construct(){
+    protected $_table = 'user';
+    public function __construct() {
         parent::__construct();
-        $this->load->database();
     }
 
-    public function listUser(){
-        $this->db->select("id, username, level");
-        $query=$this->db->get("user");
-        return $query->result_array();
+    public function getList(){
+        $this->db->select('id, username, level');
+        return $this->db->get($this->_table)->result_array();
     }
 
-    public function countAll(){
-        return $this->db->count_all("user");
+   public function countAll(){
+        return $this->db->count_all($this->_table);
     }
 
-    public function getList($total, $start){
-       $this->db->limit($total, $start);
-       $query=$this->db->get("user");
-       return $query->result_array();
+  // public function deleteUser($id){
+  //      return $this->db->delete($this->_table);
+  //  }
 }
-}
-?>
