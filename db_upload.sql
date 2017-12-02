983105f4865664a007270e3abb7815d76b0b0999
Nguyen Thuc <me@ngthuc.com>
2017-12-02 15:18:14 +0700

    Update database 201712021518

--- db_upload.sql
+++ db_upload.sql
@@ -18,39 +18,23 @@ CREATE TABLE `event` (
 
 -- Create table sinhvien
 CREATE TABLE `student` (
-  `idStudent` char(20) NOT NULL PRIMARY KEY,
+  `idStudent` char(10) NOT NULL PRIMARY KEY,
   `firstNameStudent` varchar(256) NOT NULL,
   `lastNameStudent` varchar(256) NOT NULL,
-  `idCard` char(20) NOT NULL,
+  `idCard` char(10) NOT NULL,
   `idMajor` char(10) NOT NULL,
-  `idFaculty` char(10) NOT NULL,
   `descriptionStudent` varchar(256) DEFAULT NULL,
   `syncStatus` char(10) DEFAULT 0
-  -- FOREIGN KEY (idCard) REFERENCES rfid(idCard),
-  -- FOREIGN KEY (idFaculty) REFERENCES faculty(idFaculty),
-  -- FOREIGN KEY (idMajor) REFERENCES major(idMajor)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Create table canbo
 CREATE TABLE `staff` (
-  `idStaff` char(20) NOT NULL PRIMARY KEY,
+  `idStaff` char(10) NOT NULL PRIMARY KEY,
   `firstNameStaff` varchar(256) NOT NULL,
   `lastNameStaff` varchar(256) NOT NULL,
-  `idCard` char(20) NOT NULL,
+  `idCard` char(10) NOT NULL,
   `idDepartment` char(10) NOT NULL,
-  `idFaculty` char(10) DEFAULT NULL,
   `descriptionStaff` varchar(256) DEFAULT NULL,
-  `syncStatus` char(10) DEFAULT 0
-  -- FOREIGN KEY (idCard) REFERENCES rfid(idCard),
-  -- FOREIGN KEY (idFaculty) REFERENCES faculty(idFaculty),
-  -- FOREIGN KEY (idDepartment) REFERENCES department(idDepartment)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Create table rfid
-CREATE TABLE `rfid` (
-  `idCard` char(20) NOT NULL PRIMARY KEY,
-  `numberId` char(20) NOT NULL,
-  `isStudent` char DEFAULT 0,
   `syncStatus` char(10) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -58,42 +42,37 @@ CREATE TABLE `rfid` (
 CREATE TABLE `attendance` (
   `idAttendance` int(255) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `idEvent` int(128) NOT NULL,
-  `idCard` char(20) NOT NULL,
+  `idCard` char(10) NOT NULL,
   `timeIn` datetime DEFAULT NULL,
   `timeOut` datetime DEFAULT NULL,
   `isExistNumberId` char(10) DEFAULT 0,
   `syncStatus` char(10) DEFAULT 0
-  -- FOREIGN KEY (idCard) REFERENCES rfid(idCard),
-  -- FOREIGN KEY (idEvent) REFERENCES event(idEvent)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Create table existNumberId
 CREATE TABLE `existNumberId` (
   `idKey` int(255) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `idCard` char(20) NOT NULL,
-  `numberId` char(20) DEFAULT NULL
-  -- FOREIGN KEY (idCard) REFERENCES rfid(idCard)
+  `idCard` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Create table department
 CREATE TABLE `department` (
-  `idDepartment` int(100) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `idDepartment` int(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `nameDepartment` varchar(128) NOT NULL,
   `idFaculty` char(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Create table faculty
 CREATE TABLE `faculty` (
-  `idFaculty` char(20) NOT NULL PRIMARY KEY,
+  `idFaculty` char(10) NOT NULL PRIMARY KEY,
   `nameFaculty` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Create table major
 CREATE TABLE `major` (
-  `idMajor` char(20) NOT NULL PRIMARY KEY,
+  `idMajor` char(10) NOT NULL PRIMARY KEY,
   `nameMajor` varchar(128) NOT NULL,
   `idFaculty` char(20) NOT NULL
-  -- FOREIGN KEY (idFaculty) REFERENCES faculty(idFaculty)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Create table joinEvent
@@ -101,5 +80,23 @@ CREATE TABLE `join_event` (
   `idRegister` int(255) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `numberId` char(20) NOT NULL,
   `idEvent` int(128) NOT NULL
-  -- FOREIGN KEY (idEvent) REFERENCES event(idEvent)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Add relationship
+ALTER TABLE student
+    ADD CONSTRAINT student1 FOREIGN KEY (idMajor) REFERENCES major(idMajor) ON DELETE CASCADE;
+
+ALTER TABLE staff
+    ADD CONSTRAINT staff1 FOREIGN KEY (idDepartment) REFERENCES department(idDepartment) ON DELETE CASCADE;
+
+ALTER TABLE attendance
+    ADD CONSTRAINT attendance FOREIGN KEY (idEvent) REFERENCES event(idEvent) ON DELETE CASCADE;
+
+ALTER TABLE existNumberId
+    ADD CONSTRAINT existNumberId FOREIGN KEY (idCard) REFERENCES rfid(idCard) ON DELETE CASCADE;
+
+ALTER TABLE major
+    ADD CONSTRAINT major FOREIGN KEY (idFaculty) REFERENCES faculty(idFaculty) ON DELETE NO ACTION;
+
+ALTER TABLE join_event
+    ADD CONSTRAINT join_event FOREIGN KEY (idEvent) REFERENCES event(idEvent) ON DELETE CASCADE;
