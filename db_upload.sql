4688ce130a978fb83bf23b4ae8277f2a6558755e
Nguyen Thuc <me@ngthuc.com>
2017-12-02 15:28:29 +0700

    Update database 201712021328

--- db_upload.sql
+++ db_upload.sql
@@ -33,7 +33,7 @@ CREATE TABLE `staff` (
   `firstNameStaff` varchar(256) NOT NULL,
   `lastNameStaff` varchar(256) NOT NULL,
   `idCard` char(10) NOT NULL,
-  `idDepartment` char(10) NOT NULL,
+  `idDepartment` int(5) NOT NULL,
   `descriptionStaff` varchar(256) DEFAULT NULL,
   `syncStatus` char(10) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -59,7 +59,7 @@ CREATE TABLE `existNumberId` (
 CREATE TABLE `department` (
   `idDepartment` int(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `nameDepartment` varchar(128) NOT NULL,
-  `idFaculty` char(20) DEFAULT NULL
+  `idFaculty` char(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Create table faculty
@@ -72,13 +72,13 @@ CREATE TABLE `faculty` (
 CREATE TABLE `major` (
   `idMajor` char(10) NOT NULL PRIMARY KEY,
   `nameMajor` varchar(128) NOT NULL,
-  `idFaculty` char(20) NOT NULL
+  `idFaculty` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Create table joinEvent
 CREATE TABLE `join_event` (
   `idRegister` int(255) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `numberId` char(20) NOT NULL,
+  `numberId` char(10) NOT NULL,
   `idEvent` int(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -92,11 +92,17 @@ ALTER TABLE staff
 ALTER TABLE attendance
     ADD CONSTRAINT attendance FOREIGN KEY (idEvent) REFERENCES event(idEvent) ON DELETE CASCADE;
 
-ALTER TABLE existNumberId
-    ADD CONSTRAINT existNumberId FOREIGN KEY (idCard) REFERENCES rfid(idCard) ON DELETE CASCADE;
-
 ALTER TABLE major
     ADD CONSTRAINT major FOREIGN KEY (idFaculty) REFERENCES faculty(idFaculty) ON DELETE NO ACTION;
 
 ALTER TABLE join_event
     ADD CONSTRAINT join_event FOREIGN KEY (idEvent) REFERENCES event(idEvent) ON DELETE CASCADE;
+
+ALTER TABLE department
+    ADD CONSTRAINT departmentfaculty FOREIGN KEY (idFaculty) REFERENCES faculty(idFaculty) ON DELETE CASCADE;
+
+ALTER TABLE student
+    ADD CONSTRAINT studentmajor FOREIGN KEY (idMajor) REFERENCES major(idMajor) ON DELETE SET NULL;
+
+ALTER TABLE staff
+    ADD CONSTRAINT staffdepartment FOREIGN KEY (idDepartment) REFERENCES department(idDepartment) ON DELETE SET NULL;
