e26c94603cd8cff98a383100662422b347e73ee1
Nguyen Thuc <me@ngthuc.com>
2017-12-22 01:28:44 +0700

    Add models and AJAX rfid 201712220128

---
+++ application/models/Mrole.php
@@ -0,0 +1,25 @@
+<?php
+class Mrole extends CI_Model{
+    protected $_table = 'roles';
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function getList($where = null, $id = null){
+        $this->db->select('*');
+        if ($where && $id) {
+            $this->db->where($where, $id);
+        }
+        return $this->db->get($this->_table)->result_array();
+    }
+
+    public function countAll(){
+        return $this->db->count_all($this->_table);
+    }
+
+    public function getOrgById($id){
+        $this->db->where("id", $id);
+        return $this->db->get($this->_table)->row_array();
+    }
+}
--- application/views/admin/account/rfid_admin_view.php
+++ application/views/admin/account/rfid_admin_view.php
@@ -9,46 +9,43 @@
       <thead>
         <th>STT</th>
         <th>Mã thẻ</th>
-        <th>Tên định danh</th>
         <th>Mã số định danh</th>
-        <th>Đơn vị</th>
         <th>Quản lý</th>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>0123456789</td>
-          <td>Hà Thanh Toàn</td>
-          <td>002356</td>
-          <td>Ban Giám hiệu - Trường Đại học Cần Thơ</td>
-          <td>
-            <a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-edit"></span></a>
-            <a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-remove"></span></a>
-          </td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>0770056810</td>
-          <td>Trần Cao Đệ</td>
-          <td>002321</td>
-          <td>Ban Chủ nhiệm - Khoa CNTT</td>
-          <td>
-            <a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-edit"></span></a>
-            <a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-remove"></span></a>
-          </td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>0101503659</td>
-          <td>Lê Nguyên Thức</td>
-          <td>B1400731</td>
-          <td>Công nghệ phần mềm - Khoa CNTT</td>
-          <td>
-            <a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-edit"></span></a>
-            <a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-remove"></span></a>
-          </td>
-        </tr>
+        <?php $stt = 0;
+        foreach ($content as $key => $row) {
+          $stt++;
+          echo '<tr>
+            <td>'.$stt.'</td>
+            <td>'.$row['idCard'].'</td>
+            <td>'.$row['personalID'].'</td>
+            <td>
+              <a href="#" class="btn btn-info view-info" data-id="'.$row['personalID'].'"><span class="glyphicon glyphicon-user"></span></a>
+              <a href="#" class="btn btn-primary edit-card" data-id="'.$row['idCard'].'"><span class="glyphicon glyphicon-edit"></span></a>
+              <a href="#" class="btn btn-danger delete-card" data-id="'.$row['idCard'].'"><span class="glyphicon glyphicon-remove"></span></a>
+            </td>
+          </tr>';
+        } ?>
       </tbody>
     </table>
   </div>
 </div>
+
+<!-- Load ajax -->
+<script type="text/javascript">
+$('.view-info').on('click', function() {
+   // load_ajax_update($(this).data('id'),$(this).data('monhoc'));
+   alert($(this).data('id'));
+});
+
+$('.edit-card').on('click', function() {
+   // load_ajax_update($(this).data('id'),$(this).data('monhoc'));
+   alert($(this).data('id'));
+});
+
+$('.delete-card').on('click', function() {
+   // load_ajax_update($(this).data('id'),$(this).data('monhoc'));
+   alert($(this).data('id'));
+});
+</script>
