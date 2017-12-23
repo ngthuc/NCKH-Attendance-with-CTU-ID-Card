e26e8dc09656d15ed805914128aa5a9f9d4538a7
Nguyen Thuc <me@ngthuc.com>
2017-12-23 21:16:41 +0700

    Try use JSTree 201712232116

--- application/views/dontlogin/org_view.php
+++ application/views/dontlogin/org_view.php
@@ -1,40 +1,34 @@
 <div class="container">
-  <?php
-  // foreach ($content as $key => $row) {
-  //   $parent = $this->Morg->getOrgById($row['parent']);
-  //   $json = array(
-  //     'id' => $row['id'],
-  //     'parent' => $row['parent'],
-  //     'text' => $row['name']
-  //   );
-  //   $data = json_encode($json);
-  // }
-  // echo json_encode($content);
-  ?>
   <div id="tree_list">
+    <!-- <ul> -->
+      <?php
+      // foreach ($content as $key => $row) {
+      //   $parent = $this->Morg->getOrgById($row['parent']);
+      //   if ($parent['id'] == $row['parent']) {
+      //     echo '<li>
+      //     <a href="#">'.$row['text'].'</a>
+      //     </li>';
+      //   } else if ($parent['id'] != $row['parent']) {
+      //     echo '<li>
+      //     <a href="#">'.$row['text'].'</a>
+      //     </li>';
+      //   }
+      // }
+      ?>
+    <!-- </ul> -->
     <ul>
-      <li><a href=""></a></li>
-    </ul>
+    <li>Root node
+      <ul>
+        <li>Child node 1</li>
+        <li>Child node 2</li>
+      </ul>
+    </li>
+  </ul>
   </div>
-  <script>
-  // Alternative format of the node (id & parent are required)
-  {
-    id          : "string" // required
-    parent      : "string" // required
-    text        : "string" // node text
-    state       : {
-      opened    : boolean  // is the node open
-      disabled  : boolean  // is the node disabled
-      selected  : boolean  // is the node selected
-    },
-    li_attr     : {}  // attributes for the generated LI node
-    a_attr      : {}  // attributes for the generated A node
-  }
 
-  $('#tree_list').jstree({ 'core' : {
-      'data' : <?php echo json_encode($content); ?>
-    }
-  });
+  <!-- Using TreeJS -->
+  <script>
+  $('#tree_list').jstree();
   </script>
   <?php // echo $data; ?>
 </div>
