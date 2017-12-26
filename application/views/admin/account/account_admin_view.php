bbc414ff629ba082c9757721605ff9dd1893704c
Nguyen Thuc <me@ngthuc.com>
2017-12-26 18:35:42 +0700

    Update database 201712261835

--- application/views/admin/account/account_admin_view.php
+++ application/views/admin/account/account_admin_view.php
@@ -2,7 +2,7 @@
   <div class="page-header">
     <h1>Quản lý tài khoản</h1>
     <a href="<?php echo base_url('admin/'); ?>" class="btn btn-default">Quay lại trang quản trị</a>
-    <a href="#" class="btn btn-success">Cấp phát tài khoản mới</a>
+    <button class="btn btn-success" data-toggle="modal" data-target="#new-account">Cấp phát tài khoản mới</button>
   </div>
   <div class="col-md-12">
     <table class="table" id="datatables">
@@ -54,3 +54,41 @@ $('.delete-user').on('click', function() {
    alert($(this).data('id'));
 });
 </script>
+
+<!-- Add new event -->
+<div class="modal fade" id="new-account" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+  <div class="modal-dialog">
+    <form class="form-horizontal" action="<?php echo base_url('execute/add_org');?>" method="POST">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Cấp phát tài khoản mới</h4>
+      </div>
+      <div class="modal-body">
+        <label for="uid">Tên tài khoản</label>
+        <input type="text" name="uid" id="uid" class="form-control" placeholder="Nhập tên tài khoản" required>
+        <label for="name">Tên hiển thị</label>
+        <input type="text" name="name" id="name" class="form-control" placeholder="Nhập tên bạn muốn hiển thị" required>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" class="form-control" placeholder="Nhập email của bạn" required>
+        <label for="pwd">Mật khẩu</label>
+        <input type="password" name="pwd" id="pwd" class="form-control" placeholder="Nhập tên tổ chức" required>
+        <label for="org">Quyền hạn</label>
+        <select class="form-control" name="org">
+          <option value="#"><i>Không có quản lý tại đơn vị</i></option>
+          <?php $org = $this->Mrole->getList();
+          foreach ($org as $key => $row) {
+              echo '<option value="'.$row['id'].'">'.$row['text'].'</option>';
+          } ?>
+        </select>
+        <label for="description">Mô tả</label>
+        <input type="text" name="description" id="description" class="form-control" placeholder="Nhập mô tả tổ chức" required>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="submit" name="addNew" class="btn btn-primary">Thêm mới</button>
+      </div>
+    </div>
+    </form>
+  </div>
+</div>
